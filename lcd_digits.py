f866b95ef7bbc6746999f9946df9d501ccb4620c
Marcelo Martim Marques <marcelo.martim@gmail.com>
2014-04-10 09:09:01 -0300

    Add weather forecast

--- lcd_digits.py
+++ lcd_digits.py
@@ -1,13 +1,59 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import thread
 import threading
 import lcddriver
 import datetime
+import urllib2
 import time
 from time import gmtime, strftime
+from xml.dom.minidom import parseString
 
-# Cells to be printed definition
+#Weather translate table
+weather = {}
+weather["ec"] = "Encoberto com Chuvas Isoladas"
+weather["ci"] = "Chuvas Isoladas"
+weather["c"] = "Chuva"
+weather["in"] = "Instável"
+weather["pp"] = "Poss. de Pancadas de Chuva"
+weather["cm"] = "Chuva pela Manhã"
+weather["cn"] = "Chuva a Noite"
+weather["pt"] = "Pancadas de Chuva a Tarde"
+weather["pm"] = "Pancadas de Chuva pela Manhã"
+weather["np"] = "Nublado e Pancadas de Chuva"
+weather["pc"] = "Pancadas de Chuva"
+weather["pn"] = "Parcialmente Nublado"
+weather["cv"] = "Chuvisco"
+weather["ch"] = "Chuvoso"
+weather["t"] = "Tempestade"
+weather["ps"] = "Predomínio de Sol"
+weather["e"] = "Encoberto"
+weather["n"] = "Nublado"
+weather["cl"] = "Céu Claro"
+weather["nv"] = "Nevoeiro"
+weather["g"] = "Geada"
+weather["ne"] = "Neve"
+weather["nd"] = "Não Definido"
+weather["pnt"] = "Pancadas de Chuva a Noite"
+weather["psc"] = "Possibilidade de Chuva"
+weather["pcm"] = "Possibilidade de Chuva pela Manhã"
+weather["pct"] = "Possibilidade de Chuva a Tarde"
+weather["pcn"] = "Possibilidade de Chuva a Noite"
+weather["npt"] = "Nublado com Pancadas a Tarde"
+weather["npn"] = "Nublado com Pancadas a Noite"
+weather["ncn"] = "Nublado com Poss. de Chuva a Noite"
+weather["nct"] = "Nublado com Poss. de Chuva a Tarde"
+weather["ncm"] = "Nubl. c/ Poss. de Chuva pela Manhã"
+weather["npm"] = "Nublado com Pancadas pela Manhã"
+weather["npp"] = "Nublado com Possibilidade de Chuva"
+weather["vn"] = "Variação de Nebulosidade"
+weather["ct"] = "Chuva a Tarde"
+weather["ppn"] = "Poss. de Panc. de Chuva a Noite"
+weather["ppt"] = "Poss. de Panc. de Chuva a Tarde"
+weather["ppm"] = "Poss. de Panc. de Chuva pela Manhã"
+
+# User-defined chars (binary)
 cells = {}
 cells['1'] = [0b00011,0b00011,0b00011,0b00000,0b00000,0b00000,0b00000,0b00000]
 cells['2'] = [0b00111,0b00111,0b00111,0b00111,0b00111,0b00111,0b00111,0b00111]
@@ -69,6 +115,54 @@ def print_date(local_data,lcd,rs):
    lcd.lcd_write(ord(local_data[8]),rs)
    lcd.lcd_write(ord(local_data[9]),rs)
 
+def week_day(d):
+   if ( strftime("%Y-%m-%d", gmtime()) == d ): return "Hoje"
+   temp = datetime.date(int(d.split('-')[0]),int(d.split('-')[1]),int(d.split('-')[2])).weekday()
+   if ( temp == 0 ): return "Seg"
+   if ( temp == 1 ): return "Ter"
+   if ( temp == 2 ): return "Qua"
+   if ( temp == 3 ): return "Qui"
+   if ( temp == 4 ): return "Sex"
+   if ( temp == 5 ): return "Sab"
+   if ( temp == 6 ): return "Dom"
+
+def iuv_translator(iuv):
+   if (( iuv >= 1 ) and ( iuv <= 2)): return "Baixo"
+   if (( iuv >= 3 ) and ( iuv <= 5)): return "Moderado"
+   if (( iuv >= 6 ) and ( iuv <= 7)): return "Alto"
+   if (( iuv >= 8 ) and ( iuv <= 10)): return "Muito alto"
+   return "Extremo"
+
+def parse_xml(location_code):
+   file = urllib2.urlopen('http://servicos.cptec.inpe.br/XML/cidade/'+location_code+'/previsao.xml')
+   #convert to string:
+   data = file.read()
+   #close file because we dont need it anymore:
+   file.close()
+   #parse the xml you downloaded
+   dom = parseString(data)
+   return dom
+
+def get_weather_data(dom,position):
+   if ( position >= 4 ): return []
+
+   xmlTag = dom.getElementsByTagName('dia')[position].toxml()
+   dia=xmlTag.replace('<dia>','').replace('</dia>','')
+
+   xmlTag = dom.getElementsByTagName('tempo')[position].toxml()
+   tempo=xmlTag.replace('<tempo>','').replace('</tempo>','')
+
+   xmlTag = dom.getElementsByTagName('maxima')[position].toxml()
+   max=xmlTag.replace('<maxima>','').replace('</maxima>','')
+
+   xmlTag = dom.getElementsByTagName('minima')[position].toxml()
+   min=xmlTag.replace('<minima>','').replace('</minima>','')
+
+   xmlTag = dom.getElementsByTagName('iuv')[position].toxml()
+   iuv=xmlTag.replace('<iuv>','').replace('</iuv>','')
+
+   return [dia,tempo,max,min,iuv]
+
 def run_clock(lcd,mRs,lock):
    while True:
       curr_time = strftime("%H:%M:%S", gmtime())
@@ -84,14 +178,45 @@ def run_clock(lcd,mRs,lock):
       lock.release()
       time.sleep(1)
 
-def run_banner(lcd,mRs,lock):
-   count = 0
+def run_banner(lcd,lock):
    while True:
-      lock.acquire()
-      lcd.lcd_display_string(("Bom dia %d" % count), 4)
-      lock.release()
-      count += 1
-      time.sleep(1)
+      dom = parse_xml("2586")
+      for position in [0,1,2,3]:
+         weather_data = get_weather_data(dom,position)
+         text = week_day(weather_data[0]) + ":"
+         #print text + "Max " + weather_data[2] + " Min " + weather_data[3]
+         output = (text + "Max " + weather_data[2] + " Min " + weather_data[3]).ljust(20)
+         lock.acquire()
+         lcd.lcd_display_string(output, 4)
+         lock.release()
+         time.sleep(3)
+         #print text + "IUV " + iuv_translator(weather_data[4])
+         output = (text + "UV " + iuv_translator(weather_data[4])).ljust(20)
+         lock.acquire()
+         lcd.lcd_display_string(output, 4)
+         lock.release()
+         time.sleep(3)
+         start = 0
+         end = 20 - len(text)
+         weather_now = weather[weather_data[1]]
+         #output = text + weather_now[start:(start + end)]
+         #print output
+         output = (text + weather_now[start:(start + end)]).ljust(20)
+         lock.acquire()
+         lcd.lcd_display_string(output, 4)
+         lock.release()
+         time.sleep(1)
+         start += 1
+         while ( start + end < len(weather_now)+1 ):
+            #output = text + weather_now[start:(start + end)]
+            #print output
+            output = (text + weather_now[start:(start + end)]).ljust(20)
+            lock.acquire()
+            lcd.lcd_display_string(output, 4)
+            lock.release()
+            start += 1
+            time.sleep(0.2)
+         time.sleep(0.8)
 
 def main():
    mRs = 0b00000001
@@ -106,7 +231,7 @@ def main():
 
    lcd.lcd_clear()
    t1 = thread.start_new_thread(run_clock, (lcd,mRs,lock))
-   t2 = thread.start_new_thread(run_banner, (lcd,mRs,lock))
+   t2 = thread.start_new_thread(run_banner, (lcd,lock))
    while True:
       pass
    
